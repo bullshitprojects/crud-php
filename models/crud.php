23ed6c69e3ead13bd9be24bb81407a6fac72225b
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-10 15:56:17 -0600

    :hammer: Add final functions

--- models/crud.php
+++ models/crud.php
@@ -14,9 +14,10 @@ class Data extends Connection
     $query->bindParam(':inscription_date', $modelData['inscription_date'], PDO::PARAM_STR);
     $query->bindParam(':email', $modelData['email'], PDO::PARAM_STR);
 
-    $query->execute() ? 1 : 0;
-
+    $status = $query->execute() ? 1 : 0;
     $query->closeCursor();
+
+    return $status;
   }
 
   public static function login($modelData)
@@ -32,5 +33,43 @@ class Data extends Connection
     $query->closeCursor();
 
     return $data;
+  }
+
+  public static function viewUsers()
+  {
+    $query = Connection::connect()->prepare('SELECT id, name, lastname, city, inscription_date, email FROM users');
+    $query->execute();
+
+    $data = $query->fetchAll();
+    $query->closeCursor();
+
+    return $data;
+  }
+
+  public static function deleteUser($id)
+  {
+    $query = Connection::connect()->prepare('DELETE FROM users WHERE id=:id');
+    $query->bindParam(':id', $id, PDO::PARAM_INT);
+
+    $status = $query->execute() ? 1 : 0;
+    $query->closeCursor();
+
+    return $status;
+  }
+
+  public static function updateUser($modelData)
+  {
+    $query = Connection::connect()->prepare('UPDATE users SET name=:name, lastname=:lastname, city=:city, inscription_date=:inscription_date, email=:email WHERE id=:id');
+    $query->bindParam(':name', $modelData['name'], PDO::PARAM_STR);
+    $query->bindParam(':lastname', $modelData['lastname'], PDO::PARAM_STR);
+    $query->bindParam(':city', $modelData['city'], PDO::PARAM_STR);
+    $query->bindParam(':inscription_date', $modelData['inscrption_date'], PDO::PARAM_STR);
+    $query->bindParam(':email', $modelData['email'], PDO::PARAM_STR);
+    $query->bindParam(':id', $modelData['id'], PDO::PARAM_INT);
+
+    $status = $query->execute() ? 1 : 0;
+    $query->closeCursor();
+
+    return $status;
   }
 }
