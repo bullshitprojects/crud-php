6cc3cfd8c98e6eb5db2c10ddfc867e40a7442820
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-10 18:24:22 -0600

    :sparkles: Add header module

---
+++ views/modules/header.php
@@ -0,0 +1,3 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="shortcut icon" href="https://img.icons8.com/color/96/000000/customer-skin-type-7.png" type="image/png">
