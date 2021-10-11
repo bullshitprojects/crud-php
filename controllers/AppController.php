b8152ae9ba3e2106eabab3c2f8cbd6144e2ece0a
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-11 00:06:47 -0600

    :hammer: Update app controller

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -21,12 +21,24 @@ class AppController
 
   public function newUser()
   {
-    if (isset($_POST['newUser'])) {
+    if (isset($_POST['name'])) {
       $controllerData = array('name' => $_POST['name'], 'lastname' => $_POST['lastname'], 'city' => $_POST['city'], 'inscription_date' => $_POST['inscription_date'], 'email' => $_POST['email']);
       $response = Data::newUser($controllerData);
 
       if ($response === 1) {
-        header('location:index.php?action=ok');
+        echo '
+        <script type=\'text/javascript\'>
+        $(document).ready(function(){
+          Swal.fire({
+            icon: \'success\',
+            title: \'Usuario agregado con éxito\',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          setTimeout(()=> { location.replace(\'index.php?action=users\');}, 1300);
+        });
+        </script>
+        ';
       } else {
         header('lcation:login.php');
       }
@@ -35,17 +47,28 @@ class AppController
 
   public function login()
   {
-    if (isset($_POST['login'])) {
+    if (isset($_POST['username'])) {
       $controllerData = array('username' => $_POST['username'], 'password' => $_POST['password']);
       $response = Data::login($controllerData);
 
-      if ($response['username'] === $_POST['username'] && $response['password'] === $_POST['password']) {
+      if ($response['username'] == $_POST['username']) {
         @session_start();
 
         $_SESSION['validate'] = true;
         header('location:index.php?action=users');
       } else {
-        header('location:index.php?action=fail');
+        echo '
+        <script type=\'text/javascript\'>
+        $(document).ready(function(){
+          Swal.fire({
+            icon: \'error\',
+            title: \'Datos incorrectos, intentelo de nuevo\',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        });
+        </script>
+        ';
       }
     }
   }
@@ -76,7 +99,19 @@ class AppController
       $id = $_GET['idDelete'];
       $response = Data::deleteUser($id);
       if ($response === 1) {
-        header('location:index.php?action=users');
+        echo '
+        <script type=\'text/javascript\'>
+        $(document).ready(function(){
+          Swal.fire({
+            icon: \'success\',
+            title: \'Usuario borrado con éxito\',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          setTimeout(()=> { location.replace(\'index.php?action=users\');}, 1300);
+        });
+        </script>
+        ';
       }
     }
   }
@@ -88,23 +123,23 @@ class AppController
 
     echo '
       <input type="hidden" value="', $response['id'], '" name="idEdit" />
-      <div class="form-group">
+      <div class="mb-3">
         <label for="nameEdit">Nombre</label>
         <input type="text" name="nameEdit" value="', $response['name'], '" class="form-control"  required />
       </div>
-      <div class="form-group">
+      <div class="mb-3">
         <label for="lastnameEdit">Apellido</label>
         <input type="text" name="lastnameEdit" value="', $response['lastname'], '" class="form-control"  required />
       </div>
-      <div class="form-group">
-        <label for="cityEdit">Nombre</label>
+      <div class="mb-3">
+        <label for="cityEdit">Ciudad</label>
         <input type="text" name="cityEdit" value="', $response['city'], '" class="form-control"  required />
       </div>
-      <div class="form-group">
+      <div class="mb-3">
         <label for="inscription_dateEdit">Fecha de Inscripción</label>
         <input type="text" name="inscription_dateEdit" value="', $response['inscription_date'], '" class="form-control"  required />
       </div>
-      <div class="form-group">
+      <div class="mb-3">
         <label for="emailEdit">Email</label>
         <input type="email" name="emailEdit" value="', $response['email'], '" class="form-control"  required />
       </div>
@@ -113,12 +148,24 @@ class AppController
 
   public function updateUser()
   {
-    if (isset($_POST['editUser'])) {
+    if (isset($_POST['idEdit'])) {
       $controllerData = array('id' => $_POST['idEdit'], 'name' => $_POST['nameEdit'], 'lastname' => $_POST['lastnameEdit'], 'city' => $_POST['cityEdit'], 'inscription_date' => $_POST['inscription_dateEdit'], 'email' => $_POST['emailEdit']);
       $response = Data::updateUser($controllerData);
 
       if ($response === 1) {
-        header('location:index.php?action=change');
+        echo '
+        <script type=\'text/javascript\'>
+        $(document).ready(function(){
+          Swal.fire({
+            icon: \'success\',
+            title: \'Usuario modificado con éxito\',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          setTimeout(()=> { location.replace(\'index.php?action=users\');}, 1300);
+        });
+        </script>
+        ';
       } else {
         echo '
         <script type=\'text/javascript\'>
@@ -138,7 +185,7 @@ class AppController
 
   public function searchUser()
   {
-    if ($_POST['searchUser']) {
+    if ($_POST['lastnameSearch']) {
       $lastname = $_POST['lastnameSearch'];
 
       $response = Data::searchUser($lastname);
@@ -152,6 +199,8 @@ class AppController
             <td>', $item['city'], '</td>
             <td>', $item['inscription_date'], '</td>
             <td>', $item['email'], '</td>
+            <td><a class="link-primary" href="index.php?action=edit&id=', $item['id'], '">Editar</a></td>
+            <td><a class="link-danger" onclick="javascript:return confirm("Seguro que desea eliminar este usuario?");" href="index.php?action=users&idDelete=', $item['id'], '">Eliminar</a></td>
           </tr>
         ';
       }
