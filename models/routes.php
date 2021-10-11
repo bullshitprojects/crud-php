da598ff65d7127f4a2f8a97acea9c87b8e4bf807
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-11 00:08:57 -0600

    :fire: Remove unnecesary handlers

--- models/routes.php
+++ models/routes.php
@@ -4,20 +4,14 @@ class Pages
   public static function pageRoutes($link)
   {
     switch ($link) {
-      case 'login' || 'users' || 'edit' || 'exit':
+      case 'login' || 'users' || 'edit' || 'logout' || 'add':
         $module = 'views/modules/' . $link . '.php';
         break;
       case 'index':
         $module = 'views/modules/login.php';
         break;
-      case 'ok':
-        $module = 'views/modules/login.php';
-        break;
       case 'change':
         $module = 'views/modules/users.php';
-        break;
-      case 'fail':
-        $module = 'views/modules/login.php';
         break;
       default:
         $module = 'views/modules/login.php';
