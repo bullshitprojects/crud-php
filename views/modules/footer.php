881e29047d8b5f063897125beeabbf7fa920109a
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-11 00:09:31 -0600

    :hammer: Update footer

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -2,8 +2,6 @@
   <p class="my-5 text-center p-2">WSL - Julio Canizalez - Elisa Rivas</p>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
